9a6b1874d89c615ca0f9f53f826c5434b7e0ead7
Ed Sellars <100451982+edwardsellars@users.noreply.github.com>
2023-05-03 08:17:28 +0000

    Update of old files to use vue 3

---
+++ public/javascripts/clubs.js
@@ -0,0 +1,141 @@
+// The following are to be obtained from the server
+
+let clubsArray = [
+    {
+        clubId: 1,
+        clubName: "Club A",
+        followers: 20,
+        tag: "Sport",
+        color: "blue",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis sint, doloremque fugiat quia consequatur rem voluptatem aspernatur consectetur molestiae accusantium itaque explicabo, commodi dolor corporis sunt placeat voluptates. Corporis? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia facere optio nulla consectetur iste recusandae quasi nisi, sit amet repellendus molestiae quis ut eos soluta hic ipsum ducimus, veritatis quos.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia facere optio nulla consectetur iste recusandae quasi nisi, sit amet repellendus molestiae quis ut eos soluta hic ipsum ducimus, veritatis quos.",
+        followed: true
+    },
+    {
+        clubName: "Club B",
+        followers: 5,
+        clubId: 2,
+        tag: "Technology",
+        color: "red",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis sint, doloremque fugiat quia consequatur rem voluptatem aspernatur consectetur molestiae accusantium itaque explicabo, commodi dolor corporis sunt placeat voluptates. Corporis? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia facere optio nulla consectetur iste recusandae quasi nisi, sit amet repellendus molestiae quis ut eos soluta hic ipsum ducimus, veritatis quos.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia facere optio nulla consectetur iste recusandae quasi nisi, sit amet repellendus molestiae quis ut eos soluta hic ipsum ducimus, veritatis quos.",
+        followed: true
+    },
+    {
+        clubName: "Club C",
+        followers: 0,
+        clubId: 3,
+        tag: "Faculty",
+        color: "green",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis sint, doloremque fugiat quia consequatur rem voluptatem aspernatur consectetur molestiae accusantium itaque explicabo, commodi dolor corporis sunt placeat voluptates. Corporis? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia facere optio nulla consectetur iste recusandae quasi nisi, sit amet repellendus molestiae quis ut eos soluta hic ipsum ducimus, veritatis quos.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia facere optio nulla consectetur iste recusandae quasi nisi, sit amet repellendus molestiae quis ut eos soluta hic ipsum ducimus, veritatis quos.",
+        followed: false
+    },
+    {
+        clubName: "Really Long Club Name To See How It Looks Really Long Club Name To See How It Looks",
+        followers: 6,
+        clubId: 4,
+        tag: "Sport",
+        color: "grey",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum officiis sint, doloremque fugiat quia consequatur rem voluptatem aspernatur consectetur molestiae accusantium itaque explicabo, commodi dolor corporis sunt placeat voluptates. Corporis? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia facere optio nulla consectetur iste recusandae quasi nisi, sit amet repellendus molestiae quis ut eos soluta hic ipsum ducimus, veritatis quos.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia facere optio nulla consectetur iste recusandae quasi nisi, sit amet repellendus molestiae quis ut eos soluta hic ipsum ducimus, veritatis quos.",
+        followed: false
+    }
+];
+
+clubsArray = clubsArray.map((v) => ({ ...v, isExpanded: false }));
+
+function uniqueClubTags(tag, index, array) {
+    return array.indexOf(tag) === index;
+}
+
+let clubTagsArray = clubsArray.map((item) => {
+    let club = item;
+    return club.tag;
+});
+
+clubTagsArray = clubTagsArray.filter(uniqueClubTags);
+
+
+const vueinst = Vue.createApp({
+    data() {
+        return {
+            hamburgerVisible: true,
+            numberOfClubsDisplaying: 1,
+            clubs: clubsArray,
+            clubTags: clubTagsArray
+        };
+    },
+    computed: {
+        updateNumberOfClubsDisplaying() {
+            vueinst.numberOfClubsDisplaying = vueinst.clubs.length;
+        }
+    }
+}).mount("#app");
+
+// Marks the current link page as active
+if (window.location.pathname === "/clubs.html") {
+    document.getElementById("clubs-nav").className = "current-page";
+} else if (window.location.pathname === "/feed.html") {
+    document.getElementById("feed-nav").className = "current-page";
+} else if (window.location.pathname === "/events.html") {
+    document.getElementById("events-nav").className = "current-page";
+} else if (window.location.pathname === "/profile.html") {
+    document.getElementById("profile-nav").className = "current-page";
+} else if (window.location.pathname === "/notifications.html") {
+    document.getElementById("notifications-nav").className = "current-page";
+} else if (window.location.pathname === "/manage-users.html") {
+    document.getElementById("manage-users-nav").className = "current-page";
+} else if (window.location.pathname === "/manage-clubs.html") {
+    document.getElementById("manage-clubs-nav").className = "current-page";
+}
+
+// Hamburger Menu Behaviour
+
+const hamburger = document.querySelector("#hamburger");
+const exit = document.querySelector("#exit-menu");
+const menu = document.querySelector("#popout-menu");
+
+function toggleMenuOn() {
+    hamburger.style.display = "none";
+    exit.style.display = "inline";
+    menu.style.display = "flex";
+    document.body.classList.add("stop-scrolling");
+}
+
+function toggleMenuOff() {
+    menu.style.display = "none";
+    exit.style.display = "none";
+    hamburger.style.display = "inline";
+    document.body.classList.remove("stop-scrolling");
+}
+
+if (window.location.pathname !== "/index.html" && window.location.pathname !== "/") {
+    hamburger.addEventListener("click", toggleMenuOn, false);
+    exit.addEventListener("click", toggleMenuOff, false);
+}
+
+// To reveal the back to top button
+function revealBackToTop() {
+    const backButton = document.querySelector("#back-to-top");
+    if (window.scrollY > 1000) {
+        backButton.style.display = "unset";
+    } else {
+        backButton.style.display = "none";
+    }
+}
+
+if (window.location.pathname === "/feed.html" || window.location.pathname === "/events.html" || window.location.pathname === "/clubs.html") {
+    document.addEventListener("scroll", revealBackToTop, false);
+}
+
+// In menu shows number of unread posts
+let unreadPosts = 100;
+
+function updateNotificationBadge() {
+    const notificationBadge = document.querySelector("#notifications");
+    notificationBadge.innerText = unreadPosts < 100 ? unreadPosts : "99+";
+}
+if (window.location.pathname !== "/index.html" && window.location.pathname !== "/") {
+    updateNotificationBadge();
+}
