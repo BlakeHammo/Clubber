858c6bddfd7aa406c7238b72a9137193add72f5b
Blake <blake-hammond@live.com>
2023-05-10 10:21:39 +0000

    profile page with line endings fixed

---
+++ public/javascripts/manage-profile.js
@@ -0,0 +1,11 @@
+const vueinst = new Vue({
+    el: '#app',
+    data: {
+        username: 'Username',
+        first_name: 'First Name',
+        last_name: 'Last Name',
+        email: 'Email',
+        phone: 'Phone',
+        image: ''
+    }
+});
