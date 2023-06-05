5b603ee5a9d47437232edede4f081d0000826c04
Ed Sellars <100451982+edwardsellars@users.noreply.github.com>
2023-06-05 09:36:43 +0000

    Notification settings updating correctly

--- routes/users.js
+++ routes/users.js
@@ -155,9 +155,9 @@ router.get("/notifications", function(req, res, next) {
       return;
     }
 
-    const query = `SELECT Clubs.id, Clubs.club_name, Notification.notification_setting FROM Club_members
-    INNER JOIN Clubs ON Club_members.club_id = Clubs.id
-    LEFT JOIN Notification ON Notification.club_id = Clubs.id WHERE Club_members.user_id = ?;`;
+    const query = `SELECT Clubs.id, Clubs.club_name, Notification.notification_setting FROM Clubs
+    INNER JOIN Club_members ON Club_members.club_id = Clubs.id
+    LEFT JOIN Notification ON Notification.club_id = Clubs.id AND Notification.user_id = Club_members.user_id WHERE Club_members.user_id = ?;`;
 
     connection.query(query, [req.session.user_id], function(qerr, rows, fields) {
 
