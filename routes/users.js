7d8460c6a37e55e715b54810e0ce1e588cf242ae
Ed Sellars <100451982+edwardsellars@users.noreply.github.com>
2023-06-08 11:01:07 +0000

    Javascript files update - server and client

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,43 @@
 var express = require('express');
+const multer = require("multer");
 var router = express.Router();
+var path = require('path');
 
 var nodemailer = require('nodemailer');
+
+//Assure only images are uploaded
+const checkFileType = function (file, cb) {
+  //Allowed file extensions
+  const fileTypes = /jpeg|jpg|png|gif|svg/;
+
+  //check extension names
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+
+  const mimeType = fileTypes.test(file.mimetype);
+
+  if (mimeType && extName) {
+    return cb(null, true);
+  } else {
+    cb("Error: File not an image");
+  }
+};
+
+//setting storage engine
+const storageEngine = multer.diskStorage ({
+  destination: "/workspaces/23S1_WDC_PG001_enjoyable-turns-super/public/images",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}--${file.originalname}`);
+  }
+});
+
+//initializing multer
+const upload = multer({
+  storage: storageEngine,
+  limits: { filesize: 1000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  }
+});
 
 const transporter = nodemailer.createTransport({
   host: 'smtp.ethereal.email',
@@ -79,6 +115,85 @@ router.use('/', function(req, res, next) {
   } else {
     next();
   }
+});
+
+router.post("/profile/upload", upload.single("image"), (req, res, next) => {
+  if (req.file) {
+    // Save the image path or filename in the SQL database
+    let imagePath = req.file.path.replace('/workspaces/23S1_WDC_PG001_enjoyable-turns-super/public', '');
+
+    req.pool.getConnection(function(cerr, connection) {
+      if (cerr) {
+        res.sendStatus(500);
+        return;
+      }
+
+      // Update the user's profile in the database with the image path
+      let query = 'UPDATE Users SET profile_pic_path = ? WHERE id = ?';
+
+      connection.query(query, [imagePath, req.session.user_id], function(qerr, result) {
+        connection.release();
+
+        if (qerr) {
+          res.sendStatus(500);
+          return;
+        }
+
+        res.sendStatus(200);
+      });
+    });
+  } else {
+    res.status(400).send("Please upload a valid image");
+  }
+});
+
+
+router.get("/profile", function(req, res, next) {
+  req.pool.getConnection(function(cerr, connection) {
+    if (cerr) {
+      res.sendStatus(500);
+      return;
+    }
+
+    let query = `SELECT first_name, last_name, username, email, phone_number, profile_pic_path FROM Users WHERE id = ?`;
+
+    connection.query(query, [req.session.user_id], function(qerr, rows, fields) {
+
+      connection.release();
+
+      if (qerr) {
+        res.sendStatus(500);
+        return;
+      }
+
+      let user_data = rows;
+
+      res.json(user_data);
+    });
+  });
+});
+
+
+router.post("/profile/edit", function(req, res, next) {
+  req.pool.getConnection(function(cerr, connection) {
+    if (cerr) {
+      res.sendStatus(500);
+      return;
+    }
+
+    let query = `UPDATE Users SET first_name = ?, last_name = ?, username = ?, email = ?, phone_number = ? WHERE id = ?`
+
+    connection.query(query, [req.body.first_name, req.body.last_name, req.body.username,req.body.email, req.body.phone_number, req.session.user_id], function(qerr, rows, fields) {
+
+      connection.release();
+
+      if (qerr) {
+        res.sendStatus(500);
+        return;
+      }
+      res.send();
+    });
+  });
 });
 
 router.post("/clubs/join", function(req, res, next) {
