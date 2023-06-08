29b97642813a2dc1569da015ad7ab93fbb2ffc2e
Blake <blake-hammond@live.com>
2023-06-08 11:49:41 +0000

    profile page small fixes

--- public/javascripts/manage-profile.js
+++ public/javascripts/manage-profile.js
@@ -1,11 +1,11 @@
 const vueinst = new Vue({
     el: '#app',
     data: {
-        username: '',
-        first_name: '',
-        last_name: '',
-        email: '',
-        phone: '',
+        username: 'Username',
+        first_name: '...',
+        last_name: '...',
+        email: '...',
+        phone: '...',
         image: '/images/icon.png',
         edit_profile: false,
         profile_page: true
@@ -42,10 +42,16 @@ const vueinst = new Vue({
                     if (response.length > 0) {
                         const userData = response[0];
                         this.username = userData.username;
-                        this.first_name = userData.first_name;
-                        this.last_name = userData.last_name;
                         this.email = userData.email;
-                        this.phone = userData.phone_number;
+                        if (userData.first_name){
+                            this.first_name = userData.first_name;
+                        }
+                        if (userData.last_name){
+                            this.last_name = userData.last_name;
+                        }
+                        if (userData.phone_number){
+                            this.phone = userData.phone_number;
+                        }
                         if (userData.profile_pic_path){
                             this.image = userData.profile_pic_path;
                         }
